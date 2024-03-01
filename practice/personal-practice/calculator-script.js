c743170e9c28aa11f706392a2c9c75d7c99ca648
scoolywooly <156943724+scoolywooly@users.noreply.github.com>
2024-03-01 13:45:10 -0700

    updating wirefram and calculator app

--- practice/personal-practice/calculator-script.js
+++ practice/personal-practice/calculator-script.js
@@ -4,6 +4,9 @@ let currentNum = 1; // tracks which number in the final eqaution is being edited
 let firstNum = ""; // The buttons are actually the ones that set the number and the operator.
 let operator = ""; 
 let secondNum = "";
+let thirdNum = "0";
+
+let numberString = ""; // The saved equation so far.
 let finalResult; // This is set once the calculation is completed, and this is displayed in the label box.
 
 
@@ -14,20 +17,27 @@ function appendNum(number) { // Adds a string version of the number on and then 
     if (currentNum == 1) {
         firstNum += String(number);
         document.getElementById("result").innerHTML = firstNum; //Display
+        numberString += firstNum;
     }
-    else {
+    else if (currentNum == 2) {
         secondNum = secondNum + String(number);
         document.getElementById("result").innerHTML = firstNum + " " + operator + " " + secondNum; //Display
+        numberString += secondNum;
+
+    } else if (currentNum == 3) {
+        thirdNum = thirdNum + String(number);
+        document.getElementById("result").innerHTML = firstNum + " " + operator + " " + secondNum + " " + operator + " " + thirdNum;
+        numberString += thirdNum // I guess i could do this isntead of lines 28 23 and 18. and also maybe the operator as well?
     }
     
 
 };
 
-function setOperator(chosenOperator) {
-    currentNum = 2; // Switches the number being edited to the next one.
+function setOperator(chosenOperator, number) {
+    currentNum += 1; // Switches the number being edited to the next one.
     operator = String(chosenOperator);
 
-    document.getElementById("result").innerHTML = String(firstNum + operator); //Display
+    document.getElementById("result").innerHTML = String(number + " " + operator); //Display
 
 
 };
